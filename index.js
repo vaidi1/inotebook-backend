99abe18e0fd545fec1ef14b5374a97e18d05033f
Vaidehi <106863484+vaidi1@users.noreply.github.com>
2024-01-27 00:36:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.use(cors())
 app.use(express.json())
 
 //Available routes
-app.use('/api', (req, res) => res.send({"res": "success"}))
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/note', require('./routes/note'))
 
